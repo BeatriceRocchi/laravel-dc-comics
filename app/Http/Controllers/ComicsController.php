fb6b813e0ff024763486e47cfb496772e5e518c6
BeatriceRocchi <bea.rocchi93@gmail.com>
2024-05-17 14:25:44 +0200

    [add] fillable

--- app/Http/Controllers/ComicsController.php
+++ app/Http/Controllers/ComicsController.php
@@ -31,17 +31,14 @@ class ComicsController extends Controller
     public function store(Request $request)
     {
         $form_data = $request->all();
+
         $new_comic = new Comic();
-        $new_comic->title = $form_data['title'];
-        $new_comic->slug = Helper::generateSlug($new_comic->title, new Comic());
-        $new_comic->description = $form_data['description'];
-        $new_comic->thumb = $form_data['thumb'];
-        $new_comic->price = '$' . $form_data['price'];
-        $new_comic->series = $form_data['series'];
-        $new_comic->sale_date = $form_data['sale_date'];
-        $new_comic->type = $form_data['type'];
-        $new_comic->artists = $form_data['artists'];
-        $new_comic->writers = $form_data['writers'];
+
+        $form_data['slug'] = Helper::generateSlug($form_data['title'], new Comic());
+        $form_data['price'] = '$' . $form_data['price'];
+
+        $new_comic->fill($form_data);
+
         $new_comic->save();
 
         return redirect()->route('comics.show', $new_comic);
